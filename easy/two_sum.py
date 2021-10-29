4cde027b134b310e2a20de9383f6107322df3127
Aleksei Burov <burov_alexey@mail.ru>
2021-10-29 07:08:22 +0500

    [easy] Two sum

---
+++ easy/two_sum.py
@@ -0,0 +1,25 @@
+"""Two sum.
+
+https://leetcode.com/problems/two-sum/
+"""
+
+class Solution:
+
+    def twoSum(self, nums: List[int], target: int) -> List[int]:  # noqa
+        index_by_value = {}
+        for i, second in enumerate(nums):
+            first = target - second
+            if first in index_by_value:
+                break
+            index_by_value[second] = i
+        return [index_by_value[first], i]
+
+
+def test():
+    assert Solution().twoSum([2, 7, 11, 15], 9) == [0, 1]
+    assert Solution().twoSum([3, 2, 4], 6) == [1, 2]
+    assert Solution().twoSum([3, 3], 6) == [0, 1]
+
+
+if __name__ == '__main__':
+    test()
