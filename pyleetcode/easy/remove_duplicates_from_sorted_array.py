72930db0162228ff85a314770b0da5e957efa5e0
Aleksei Burov <burov_alexey@mail.ru>
2022-01-17 09:00:07 +0500

    [easy] 26. Remove Duplicates from Sorted Array. Reconsider (Python)

--- pyleetcode/easy/remove_duplicates_from_sorted_array.py
+++ pyleetcode/easy/remove_duplicates_from_sorted_array.py
@@ -3,32 +3,19 @@
 https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 """
 
-from typing import Optional, List, Any
+from typing import Optional, List, Any  # noqa: F401
 
 
 class Solution:
 
-    def removeDuplicates(self, nums: List[int]) -> int:
-        length = len(nums)
-        if length == 0:
-            return 0
-        if len(nums) == 1:
-            return 1
+    def removeDuplicates(self, nums: List[int]) -> int:  # noqa: N802
+        ind = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[ind-1]:
+                nums[ind] = nums[i]
+                ind += 1
 
-        # We have two values in the list at this point.
-        stop_pos = length
-        last = nums[0]
-        cur_pos = 1
-        while cur_pos < stop_pos:
-            if nums[cur_pos] == last:
-                # move a duplicate element to the end of the list
-                nums.append(nums.pop(cur_pos))
-                stop_pos -= 1
-            else:
-                last = nums[cur_pos]
-                cur_pos += 1
-
-        return stop_pos
+        return ind
 
     def process(self, *args, **kwargs):
         return self.removeDuplicates(*args, **kwargs)
