df4eab09df3cbface2c3a3c529b26f363df7265d
Aleksei Burov <burov_alexey@mail.ru>
2022-01-20 04:48:17 +0500

    [easy] 217. Contains Duplicate. Accepted#1 (Python: 440 ms)

--- pyleetcode/easy/contains_duplicate.py
+++ pyleetcode/easy/contains_duplicate.py
@@ -3,13 +3,24 @@
 https://leetcode.com/problems/contains-duplicate/
 """
 
-from typing import Optional, List, Any  # noqa: F401
+from typing import Optional, List, Set, Any  # noqa: F401
 
 
 class Solution:
 
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return False
+        if len(nums) == 1:
+            return False
+
+        existed: Set[int] = set()
+        for el in nums:
+            if el not in existed:
+                existed.add(el)
+            else:
+                break
+        else:
+            return False
+        return True
 
     def process(self, *args, **kwargs) -> bool:  # noqa: N802
         return self.containsDuplicate(*args, **kwargs)
