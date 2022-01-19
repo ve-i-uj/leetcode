23997f91082a19d1f46f6cc61ad89c1ff9f7a199
Aleksei Burov <burov_alexey@mail.ru>
2022-01-20 04:40:14 +0500

    [easy] 217. Contains Duplicate. Test cases and an empty method of the solution (Python)

---
+++ pyleetcode/easy/contains_duplicate.py
@@ -0,0 +1,15 @@
+"""217. Contains Duplicate
+
+https://leetcode.com/problems/contains-duplicate/
+"""
+
+from typing import Optional, List, Any  # noqa: F401
+
+
+class Solution:
+
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return False
+
+    def process(self, *args, **kwargs) -> bool:  # noqa: N802
+        return self.containsDuplicate(*args, **kwargs)
---
+++ pyleetcode/tests/test_easy/test_contains_duplicate.py
@@ -0,0 +1,21 @@
+"""Unit tests of "217. Contains Duplicate".
+
+https://leetcode.com/problems/contains-duplicate/
+"""
+
+import pytest
+
+from easy.contains_duplicate import Solution
+
+
+TEST_CASES = [
+    ([1, 2, 3, 1], True),
+    ([1, 2, 3, 4], False),
+    ([1, 1, 1, 3, 3, 4, 3, 2, 4, 2], True),
+]  # noqa
+
+
+@pytest.mark.parametrize('nums, expected', TEST_CASES)
+def test(nums: list[int], expected: bool):
+    actual = Solution().process(nums)
+    assert actual is expected
