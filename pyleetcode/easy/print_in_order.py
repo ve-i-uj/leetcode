6f5897c1c3d9a3de01fec8861f0ca2cc88e3fd59
Aleksei Burov <burov_alexey@mail.ru>
2022-01-27 07:26:49 +0500

    [easy] 1114. Print in Order. Accepted#1 (Python: 102 ms)

--- pyleetcode/easy/print_in_order.py
+++ pyleetcode/easy/print_in_order.py
@@ -3,29 +3,33 @@
 https://leetcode.com/problems/print-in-order/
 """
 
-import threading
+from threading import Lock, Event
 
-from typing import Optional, List, Any  # noqa: F401
+from typing import Callable, Optional  # noqa: F401
 
 
 class Foo:
 
-    def __init__(self):
-        self._lock = threading.Lock()
+    def __init__(self, test_final_event: Optional[Event] = None) -> None:
+        self._lock_1 = Lock()
+        self._lock_2 = Lock()
 
-    def first(self, printFirst: 'Callable[[], None]') -> None:
+        self._test_final_event = test_final_event or Event()
 
-        # printFirst() outputs "first". Do not change or remove this line.
+        self._lock_1.acquire()
+        self._lock_2.acquire()
+
+    def first(self, printFirst: Callable[[], None]) -> None:
         printFirst()
+        self._lock_1.release()
 
+    def second(self, printSecond: Callable[[], None]) -> None:
+        with self._lock_1:
+            printSecond()
+        self._lock_2.release()
 
-    def second(self, printSecond: 'Callable[[], None]') -> None:
+    def third(self, printThird: Callable[[], None]) -> None:
+        with self._lock_2:
+            printThird()
 
-        # printSecond() outputs "second". Do not change or remove this line.
-        printSecond()
-
-
-    def third(self, printThird: 'Callable[[], None]') -> None:
-
-        # printThird() outputs "third". Do not change or remove this line.
-        printThird()
+        self._test_final_event.set()
