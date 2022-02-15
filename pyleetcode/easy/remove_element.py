ee3560076c0a9b8921af82752708c57156e119cb
Aleksei Burov <burov_alexey@mail.ru>
2022-02-15 11:25:30 +0500

    [easy] 27. Remove Element. Tests and an empty method of the solution (Python)

---
+++ pyleetcode/easy/remove_element.py
@@ -0,0 +1,15 @@
+"""27. Remove Element
+
+https://leetcode.com/problems/remove-element/
+"""
+
+from typing import Optional, List, Any  # noqa: F401
+
+
+class Solution:
+
+    def process(self, *args, **kwargs) -> int:  # noqa: N802
+        return self.removeElement(*args, **kwargs)
+
+    def removeElement(self, nums: List[int], val: int) -> int:   # noqa: N802
+        pass
