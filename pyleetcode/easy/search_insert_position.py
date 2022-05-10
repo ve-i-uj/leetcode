2e34a6b0aae01b99c18ddfbd3c9ec0168e9e9735
Aleksei Burov <burov_alexey@mail.ru>
2022-05-10 09:18:11 +0500

    [easy] 35. Search Insert Position. Solved by a custom algorithm (Python: 88 ms)

--- pyleetcode/easy/search_insert_position.py
+++ pyleetcode/easy/search_insert_position.py
@@ -3,7 +3,6 @@
 https://leetcode.com/problems/search-insert-position/
 """
 
-import bisect
 from typing import Optional, List, Any  # noqa: F401
 
 
@@ -13,4 +12,21 @@ class Solution:
         return self.searchInsert(*args, **kwargs)
 
     def searchInsert(self, nums: List[int], target: int) -> int:
-        return bisect.bisect_left(nums, target)
+        if not nums:
+            return 0
+        if target < nums[0]:
+            return 0
+        if target > nums[-1]:
+            return len(nums)
+
+        left = -1
+        right = len(nums)
+        while left < right - 1:
+            middle = left + (right - left) // 2
+            if nums[middle] == target:
+                return middle
+            if target > nums[middle]:
+                left = middle
+            else:
+                right = middle
+        return right
