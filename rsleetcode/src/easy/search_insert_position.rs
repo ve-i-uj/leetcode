9d27ac2667c158eb6014d231b319a6e55ae95b9e
Aleksei Burov <burov_alexey@mail.ru>
2022-05-10 07:35:36 +0500

    [easy] 35. Search Insert Position. Solved by "Vec::binary_search" Accepted#1 (Rust: 0 ms)

--- rsleetcode/src/easy/search_insert_position.rs
+++ rsleetcode/src/easy/search_insert_position.rs
@@ -7,7 +7,10 @@ pub struct Solution {
 
 impl Solution {
     pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-        -1
+        match nums.binary_search(&target) {
+            Ok(idx) => idx as i32,
+            Err(idx) => idx as i32,
+        }
     }
 }
 
