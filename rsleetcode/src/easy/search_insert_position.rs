0667d3dcd3e5570799f51c35c1f7865abfbe2b5c
Aleksei Burov <burov_alexey@mail.ru>
2022-05-13 09:23:02 +0500

    [easy] 35. Search Insert Position. Solved by a custom algorithm (Rust: 0 ms)

--- rsleetcode/src/easy/search_insert_position.rs
+++ rsleetcode/src/easy/search_insert_position.rs
@@ -7,10 +7,37 @@ pub struct Solution {
 
 impl Solution {
     pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-        match nums.binary_search(&target) {
-            Ok(idx) => idx as i32,
-            Err(idx) => idx as i32,
+        let first = match nums.first() {
+            Some(el) => el,
+            None => return -1,
+        };
+        if &target <= first {
+            return 0
         }
+        let last = match nums.last() {
+            Some(el) => el,
+            None => return -1,
+        };
+        if &target > last {
+            return nums.len() as i32
+        }
+        let mut left = 0;
+        let mut right = nums.len() - 1;
+        while left <= right{
+            let middle = left + (right - left) / 2;
+            let x = nums.get(middle).unwrap();
+
+            if x == &target {
+                return middle as i32;
+            }
+            if &target < x {
+                right = middle - 1;
+            }
+            else {
+                left = middle + 1;
+            }
+        }
+        return left as i32;
     }
 }
 
@@ -20,7 +47,7 @@ mod tests {
 
     #[test]
     fn test_1() {
-        assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 5), 2)
+        assert_eq!(Solution::search_insert(vec![1, 3, 5, 6, 7], 4), 2)
     }
 
     #[test]
@@ -33,4 +60,13 @@ mod tests {
         assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 7), 4)
     }
 
+    #[test]
+    fn test_4() {
+        assert_eq!(Solution::search_insert(vec![1, 3], 1), 0)
+    }
+
+    #[test]
+    fn test_5() {
+        assert_eq!(Solution::search_insert(vec![1, 3, 5, 6], 5), 2)
+    }
 }
