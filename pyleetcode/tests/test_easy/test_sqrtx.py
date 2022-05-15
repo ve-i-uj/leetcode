0a065cb58521eabfd0fa2f06a0811e9d5b4debf3
Aleksei Burov <burov_alexey@mail.ru>
2022-05-15 13:54:13 +0500

    [easy] 69. Sqrt(x). Using binary search. Accepted#1 (Python: 52 ms)

---
+++ pyleetcode/tests/test_easy/test_sqrtx.py
@@ -0,0 +1,23 @@
+"""Unit tests of "69. Sqrtx".
+
+https://leetcode.com/problems/sqrtx/
+"""
+
+import pytest
+
+from easy.sqrtx import Solution
+
+
+TEST_CASES = [
+    (4, 2),
+    (8, 2),
+    (0, 0),
+    (2, 1),
+    (9, 3),
+]  # noqa
+
+
+@pytest.mark.parametrize('input_, output', TEST_CASES)
+def test(input_: int, output: int):
+    res = Solution().process(input_)
+    assert res == output
