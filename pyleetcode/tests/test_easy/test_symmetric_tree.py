43dd43af36165399bdd783dc18a9b0ef0fdd8e9d
Aleksei Burov <burov_alexey@mail.ru>
2022-05-23 10:06:58 +0500

    [easy] 101. Symmetric Tree. Iteration + 2 stacks. Accepted#1 (Python: 68 ms)

---
+++ pyleetcode/tests/test_easy/test_symmetric_tree.py
@@ -0,0 +1,22 @@
+"""Unit tests of "101. Symmetric Tree".
+
+https://leetcode.com/problems/symmetric-tree/
+"""
+
+from typing import Optional
+import pytest
+
+from easy.symmetric_tree import Solution, TreeNode
+
+
+TEST_CASES = [
+    ([1, 2, 2, 3, 4, 4, 3], True),
+    ([1, 2, 2, None, 3, None, 3], False),
+]  # noqa
+
+
+@pytest.mark.parametrize('vals, output', TEST_CASES)
+def test(vals: list[Optional[int]], output: bool):
+    root = TreeNode.new(vals)
+    res = Solution().process(root)
+    assert res == output
