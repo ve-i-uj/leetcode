73bca82b9e321594643e8e8c9f68660c55346c7f
Aleksei Burov <burov_alexey@mail.ru>
2022-10-08 19:49:29 +0500

    7. Reverse Integer. Digits to stacks + border checks. Accepted#2 (Python: 65 ms)

--- pyleetcode/medium/reverse_integer.py
+++ pyleetcode/medium/reverse_integer.py
@@ -19,14 +19,36 @@ class Solution:
 
         sign = 1 if x >= 0 else -1
         x = abs(x)
-        n = int(math.log(x, 10) + 1)
+        n = int(math.log(x, 10))
         digit = x
-        res = 0
-        for i in range(n - 1, -1, -1):
+        stack = []
+        for i in range(n, -1, -1):
             digit, the_rest = divmod(digit, 10)
-            res += the_rest * (10 ** i)
+            stack.append(the_rest)
 
-            if sign * res < MIN or sign * res > MAX:
+        max_n = int(math.log(MAX, 10) + 1)
+
+        if len(stack) < max_n:
+            return sign * sum(x * (10 ** i) for i, x in enumerate(reversed(stack)))
+        if len(stack) > max_n:
+            return 0
+
+        # len(stack) == max_n
+        digit = MIN * -1
+        max_number_stack = []
+        for i in range(max_n - 1, -1, -1):
+            digit, the_rest = divmod(digit, 10)
+            max_number_stack.append(the_rest)
+
+        for i, max_i in zip(stack, reversed(max_number_stack)):
+            if i > max_i:
                 return 0
+            elif i < max_i:
+                return sign * sum(x * (10 ** i) for i, x in enumerate(reversed(stack)))
 
-        return res * sign
+        # x == MIN * -1, check the sign
+        if sign > 0:
+            # The value is outside
+            return 0
+
+        return sign * sum(x * (10 ** i) for i, x in enumerate(reversed(stack)))
