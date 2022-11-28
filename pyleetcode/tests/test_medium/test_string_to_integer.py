fed88f192a6d0fa6b98e59a428ae5518c534ee2b
Aleksei Burov <burov_alexey@mail.ru>
2022-11-28 11:41:04 +0500

    8. String to Integer. "res = res * 10 + i" Accepted#1 (Python: 36 ms)

--- pyleetcode/tests/test_medium/test_string_to_integer.py
+++ pyleetcode/tests/test_medium/test_string_to_integer.py
@@ -9,8 +9,13 @@ from pyleetcode.medium.string_to_integer import Solution
 
 
 TEST_CASES = [
+    ('42', 42),
+    ('   -42', -42),
+    ('4193 with words', 4193),
 ]  # noqa
-@pytest.mark.skip(reason='Test template')
-@pytest.mark.parametrize('', TEST_CASES)
-def test():
-    res = Solution().process()
+
+
+@pytest.mark.parametrize('s, output', TEST_CASES)
+def test(s: str, output: int):
+    res = Solution().process(s)
+    assert res == output
