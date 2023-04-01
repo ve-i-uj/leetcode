7374026d4171759f4cfb4b64d17f6965b182ac3e
Aleksei Burov <burov_alexey@mail.ru>
2023-04-01 11:31:41 +0500

    36. Valid Sudoku. Hash, tuples, and on-the-fly validation. Accepted#4 (Python: 85 ms)

--- leetcode/pyleetcode/pyleetcode/medium/valid_sudoku.py
+++ leetcode/pyleetcode/pyleetcode/medium/valid_sudoku.py
@@ -12,13 +12,27 @@ class Solution:
         return self.isValidSudoku(*args, **kwargs)
 
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        entities = []
+        rows = set()
+        columns = set()
+        boxes = set()
         for r_i, row in enumerate(board):
             for c_i, value in enumerate(row):
                 if value == '.':
                     continue
-                entities.append(('r', r_i, value))
-                entities.append(('c', c_i, value))
-                entities.append(('b', r_i // 3, c_i // 3, value))
 
-        return len(entities) == len(set(entities))
+                r_v = ('r', r_i, value)
+                if r_v in rows:
+                    return False
+                rows.add(r_v)
+
+                c_v = ('c', c_i, value)
+                if c_v in columns:
+                    return False
+                columns.add(c_v)
+
+                b_v = ('b', r_i // 3, c_i // 3, value)
+                if b_v in boxes:
+                    return False
+                boxes.add(b_v)
+
+        return True
