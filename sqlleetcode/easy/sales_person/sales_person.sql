17d3180187fd8715e56a639132f09b1b08faf91a
Aleksei Burov <burov_alexey@mail.ru>
2023-04-08 15:18:29 +0500

    607. Sales Person. Where by id in sub select with a join. Accepted#2 (SQL 3450 ms)

--- sqlleetcode/easy/sales_person/sales_person.sql
+++ sqlleetcode/easy/sales_person/sales_person.sql
@@ -4,11 +4,9 @@
 
 SELECT name
 FROM SalesPerson sp
-WHERE sp.name NOT IN (
-  SELECT sp.name
-  FROM SalesPerson sp
-    JOIN Orders o
-      ON sp.sales_id = o.sales_id
+WHERE sp.sales_id NOT IN (
+  SELECT o.sales_id
+  FROM Orders o
     JOIN Company c
       ON c.com_id = o.com_id
   WHERE c.name = "RED"
