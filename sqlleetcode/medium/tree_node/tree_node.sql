bfa8e280a0fac2147a410260316cdab399b80c7b
Aleksei Burov <burov_alexey@mail.ru>
2023-04-15 04:36:56 +0500

    608. Tree Node. Case and subquery. Accepted#2 (SQL 10 rows)

--- sqlleetcode/medium/tree_node/tree_node.sql
+++ sqlleetcode/medium/tree_node/tree_node.sql
@@ -1,35 +1,7 @@
--- 94. Binary Tree Inorder Traversal
---
--- https://leetcode.com/problems/tree-node/
-
-WITH tree AS (
-	SELECT *
-	FROM Tree t
-), root AS (
-	SELECT tree.id, "Root" AS "type"
-	FROM tree
-	WHERE tree.p_id IS NULL
-), leaves AS (
-  SELECT tree.id, "Leaf" AS "type"
-  FROM tree
-  WHERE tree.id NOT IN (SELECT tree.p_id FROM tree WHERE tree.p_id IS NOT NULL)
-    AND tree.id NOT IN (SELECT root.id FROM root)
-), nodes AS (
-  SELECT tree.id, "Inner" AS "type"
-  FROM tree
-  WHERE tree.id NOT IN (
-      SELECT root.id
-      FROM root
-    UNION
-      SELECT leaves.id
-      FROM leaves
-  )
-)
-  SELECT *
-  FROM root
-UNION
-  SELECT *
-  FROM nodes
-UNION
-  SELECT *
-  FROM leaves
+SELECT id,
+  CASE
+    WHEN p_id IS NULL THEN "Root"
+    WHEN id IN (SELECT p_id FROM Tree) THEN "Inner"
+    ELSE "Leaf"
+  END "type"
+FROM Tree t
