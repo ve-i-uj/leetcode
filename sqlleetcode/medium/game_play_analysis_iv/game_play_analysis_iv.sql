af26a79c6dc67078862f71ded3874e0a15105505
Aleksei Burov <burov_alexey@mail.ru>
2023-04-20 13:00:17 +0500

    550. Game Play Analysis IV. Boolean if event_date + 1 = event_date. Accepted#2 (SQL 2013 rows)

--- sqlleetcode/medium/game_play_analysis_iv/game_play_analysis_iv.sql
+++ sqlleetcode/medium/game_play_analysis_iv/game_play_analysis_iv.sql
@@ -2,20 +2,17 @@
 --
 -- https://leetcode.com/problems/game-play-analysis-iv/
 
-WITH total AS (
-  SELECT COUNT( DISTINCT(player_id) ) AS "value"
+WITH cte AS (
+  SELECT player_id,
+         (DATE_ADD(MIN(event_date) OVER( PARTITION BY player_id ), INTERVAL 1 DAY) = event_date) AS mtched
   FROM Activity a
 ), players AS (
-  SELECT COUNT(a1.player_id) AS "value"
-  FROM Activity a1
-    JOIN (
-      SELECT player_id,
-             MIN(event_date) AS "event_date"
-      FROM Activity a
-      GROUP BY player_id
-    ) a2 ON a1.player_id = a2.player_id
-      AND a1.event_date = DATE_ADD(a2.event_date, INTERVAL 1 DAY)
+  SELECT COUNT(player_id) AS "value"
+  FROM cte
+  WHERE mtched = 1
+), total AS (
+  SELECT COUNT( DISTINCT player_id  ) "value"
+  FROM Activity
 )
-SELECT ROUND( p.value / t.value, 2 ) AS "fraction"
-FROM players p,
-     total t
+SELECT ROUND( players.value / total.value, 2 ) AS "fraction"
+FROM players, total
