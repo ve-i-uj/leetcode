954096569b2a7891859a4b83a2a18bba6cf414f5
Aleksei Burov <burov_alexey@mail.ru>
2023-05-08 09:42:54 +0500

    601. Human Traffic of Stadium. The gaps and islands problem: filtered rows + rank, subselect for target diff . Accepted#2 (SQL 12 rows from 8)

--- leetcode/sqlleetcode/hard/human_traffic_of_stadium/human_traffic_of_stadium.sql
+++ leetcode/sqlleetcode/hard/human_traffic_of_stadium/human_traffic_of_stadium.sql
@@ -4,26 +4,17 @@
 
 WITH cte AS (
   SELECT *,
-         s1.people >= 100 AS value
-  FROM Stadium s1
-), islands AS (
-  SELECT t1.id,
-         t1.visit_date,
-         t1.people,
-         t1.id - ROW_NUMBER() OVER ( ORDER BY id ) AS 'diff'
-  FROM cte t1
-    LEFT OUTER JOIN cte t2
-      ON t1.value = t2.value - 1
-  WHERE t2.value IS NULL
-), mx_mn AS (
-  SELECT *,
-         MAX(id) OVER( PARTITION BY diff ) AS mx,
-         MIN(id) OVER( PARTITION BY diff ) AS mn
-  FROM islands
+         s.id - RANK() OVER(ORDER BY id) diff
+  FROM Stadium s
+  WHERE s.people > 99
 )
 SELECT id,
        visit_date,
        people
-FROM mx_mn
-WHERE mx - mn + 1 >= 3
-ORDER BY visit_date
+FROM cte
+WHERE diff IN (
+  SELECT diff
+  FROM cte
+  GROUP BY diff
+  HAVING COUNT(diff) > 2
+)
