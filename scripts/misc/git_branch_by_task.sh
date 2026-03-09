49eee4fa9785ddef8ee71123ce55b345b3cc7765
Aleksei Burov <burov_alexey@mail.ru>
2026-03-09 18:04:25 +0500

    Update scripts creating templates

--- scripts/misc/git_branch_by_task.sh
+++ scripts/misc/git_branch_by_task.sh
@@ -8,7 +8,7 @@ task_name="$1"
 task_name=$( echo "$task_name" | tr -d "'" )
 
 number=$( echo $task_name | grep -oE "^[0-9]{1,5}" )
-formated_name=$("$curr_dir/one_word_name_by_task.sh" "$task_name")
+formated_name=$(bash "$curr_dir/one_word_name_by_task.sh" "$task_name")
 git_branch_name="feature-$number-$formated_name"
 
 echo "$git_branch_name"
--- scripts/misc/url_by_task.sh
+++ scripts/misc/url_by_task.sh
@@ -6,7 +6,7 @@ curr_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 task_name="$1"
 
-formated_name=$("$curr_dir/one_word_name_by_task.sh" "$task_name")
+formated_name=$(bash "$curr_dir/one_word_name_by_task.sh" "$task_name")
 task_url="https://leetcode.com/problems/$formated_name/"
 
 echo "$task_url"
